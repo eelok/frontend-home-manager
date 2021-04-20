743a8db5cabdb1c05df944f37c334f7af0b0c96b
eelok <eelok@mail.ru>
2021-04-20 21:30:03 +0200

    complete fetching counter from server

--- src/components/displayCounters/DisplayCounters.jsx
+++ src/components/displayCounters/DisplayCounters.jsx
@@ -1,33 +1,39 @@
 import React, {useState, useEffect} from 'react';
-import './../../constants/url';
+import {URL} from '../../constants/url';
 
 const DisplayCounters = (props) => {
     const type = props.match.params.type;
-    const [counter, setCounter] = useState(0);
+    const [counters, setCounters] = useState([]);
 
-    useEffect(async() => {
-        const response = await fetch(`${URL}/counters/${type}`);
-        const data = await response.json();
-        setCounter({
-            "type": type,
-            "value": data.value,
-            "timestamp": data.timestamp
-        })
+    useEffect(() => {
+        console.log(`${URL}/counter-data/${type}`);
+        fetch(`${URL}/counter-data/${type}`)
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw response;
+            })
+            .then(data => {
+                console.log("FROM SERVER", data);
+                setCounters(data);
+            })
+            .catch(error => {
+                console.error("Error fetching data ", error);
+            })
     }, []);
 
 
     return (
         <div>
             {
-                counter.map(item => (
+                counters.map(item =>
                     <div key={item.id}>
                         <div>{item.type}</div>
                         <div>{item.value}</div>
                         <div>{item.timestamp}</div>
                     </div>
-                ))
-            }
-
+                )}
         </div>
     )
 
